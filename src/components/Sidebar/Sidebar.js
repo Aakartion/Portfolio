9f7e876947f7b379fe14fad005351ca6a4d877d9
aakarshan <aakartionsimkhada@gmail.com>
2024-05-12 20:25:33 -0500

    Project Logo is added with its animation

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -3,7 +3,12 @@ import "./Sidebar.scss";
 import LogoS from "../../assets/images/RedA.png";
 import LogoSubtitle from "../../assets/images/logo_sub.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEnvelope, faHome, faUser } from "@fortawesome/free-solid-svg-icons";
+import {
+  faEnvelope,
+  faHome,
+  faUser,
+  faLaptopFile,
+} from "@fortawesome/free-solid-svg-icons";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 const Sidebar = () => {
@@ -32,6 +37,14 @@ const Sidebar = () => {
           to="/contact"
         >
           <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          className="project-link"
+          to="/contact"
+        >
+          <FontAwesomeIcon icon={faLaptopFile} color="#4d4d4e" />
         </NavLink>
       </nav>
       <ul>
